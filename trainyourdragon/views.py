f67acdb6ca30e18b322a77b80067e5987dd609c0
Fobos <fobos.tia@gmail.com>
2024-08-09 01:19:21 +0700

    Templates, views and CSS modified.

--- trainyourdragon/views.py
+++ trainyourdragon/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from .forms import PostForm
 from .models import Post
 
 def post_list(request):
@@ -12,5 +13,30 @@ def post_detail(request, pk):
     return render(request, 'trainyourdragon/post_detail.html', {'post': post})
 
 
-def currentuser(request):
-    return request.user
+def post_new(request):
+    form = PostForm(request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'trainyourdragon/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'trainyourdragon/post_edit.html', {'form': form})
