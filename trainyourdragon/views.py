f9564f98304a3c6d72651a0b9e1481f35b9fd956
Fobos <fobos.tia@gmail.com>
2024-08-21 23:30:38 +0700

    Templates, views and CSS modified.

--- trainyourdragon/views.py
+++ trainyourdragon/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from .forms import PostForm
 from .models import Post
@@ -14,13 +15,13 @@ def post_detail(request, pk):
     return render(request, 'trainyourdragon/post_detail.html', {'post': post})
 
 
+@login_required
 def post_new(request):
     form = PostForm(request.POST)
     if request.method == "POST":
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-#            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -28,6 +29,7 @@ def post_new(request):
     return render(request, 'trainyourdragon/post_edit.html', {'form': form})
 
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -35,7 +37,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-#            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -43,11 +44,13 @@ def post_edit(request, pk):
     return render(request, 'trainyourdragon/post_edit.html', {'form': form})
 
 
+@login_required
 def post_draft_list(request):
     posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
     return render(request, 'trainyourdragon/post_draft_list.html', {'posts': posts})
 
 
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
@@ -55,6 +58,7 @@ def post_publish(request, pk):
     return redirect('post_detail', pk=pk)
 
 
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
